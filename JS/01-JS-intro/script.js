790bf9e42b63c77a62fcacc8cae0cfee61e3a891
intelucas69 <lucas.norback123@outlook.com>
2025-09-15 14:49:49 +0200

    git commands file added

---
+++ JS/01-JS-intro/script.js
@@ -0,0 +1,15 @@
+const myName = "Lucas";
+let myAge = 19;
+let isStudent = true;
+console.log("Name:", myName);
+console.log("Age:", myAge);
+console.log("Is Student:", isStudent);
+
+myAge += 1;
+console.log("New Age: happy birthday", myAge);
+isStudent = false;
+console.log("Is Student:", isStudent);
+
+console.log(
+  `My name is ${myName}, I am ${myAge} years old, and it is ${isStudent} that I am a student.`
+);
